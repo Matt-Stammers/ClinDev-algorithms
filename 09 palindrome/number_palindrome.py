6fa4dd37d2f4831d5aa891c51e188fc12b81b0b7
Matt Stammers <matthewstammers@googlemail.com>
2017-10-07 16:46:10 +0100

    Create number_palindrome.py

---
+++ 09 palindrome/number_palindrome.py
@@ -0,0 +1,42 @@
+# import unittest
+
+# The function below can check if a number is a palindrome and returns a bool.
+
+def digits(x):
+  
+  digs = []
+  while x != 0:
+    div, mod = divmod(x,10)
+    digs.append(mod)
+    x = div
+  return digs
+
+def is_palindrome(x):
+  digs = digits(x)
+  for f, r in zip(digs,reversed(digs)):
+    if f != r:
+      return False
+  return True
+  
+print(is_palindrome(1234)) # will return False
+print(is_palindrome(1234321)) # will return True
+print(is_palindrome(3456789)) # will return False
+print(is_palindrome(6789876)) # will return True
+
+'''
+class Tests(unittest.TestCase):
+  def test_negative(self):
+    self.assertFalse(is_palindrome(1234))
+  
+  def test_positive(self):
+    self.assertTrue(is_palindrome(1234321))
+  
+  def single_digit(self):
+    for i in range(10):
+      self.assertTrue(True, is_palindrome(i))
+
+'''
+
+# For pdb debugging      
+# if name == __main__:
+#   unittest.main()
